b3f759fe6e3b6d4112072608f5e935a6b005907d
jianghongbao <18218462815@163.com>
2018-03-03 10:52:41 +0800

    Optimization of clipimage function

--- HBProgressor/HBClipViewController.swift
+++ HBProgressor/HBClipViewController.swift
@@ -10,14 +10,11 @@ import UIKit
 
 class HBClipViewController: UIViewController {
 
-    fileprivate var imageView = UIImageView()
+    
     fileprivate var shelterView = HBShelterView()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        imageView.image = #imageLiteral(resourceName: "lufei")
-        imageView.frame = view.bounds
-        view.addSubview(imageView)
 
         shelterView = HBShelterView.init(frame: view.bounds)
         view.addSubview(shelterView)
@@ -41,18 +38,16 @@ enum HBSkipInitialType {
     case bottom
     case bottomRight
 }
-class HBShelterView: UIView, CAAnimationDelegate {
-    
-    fileprivate var scopeBezierPath : UIBezierPath = UIBezierPath()
-    fileprivate var clipBezierPath : UIBezierPath = UIBezierPath()
-    fileprivate var scopeLayer : CAShapeLayer = CAShapeLayer()
+class HBShelterView: UIView {
+
     fileprivate var centerAnchorPoint : CGPoint = .zero
     fileprivate var previousTouchPoint : CGPoint = .zero
     fileprivate var currentTouchPoint : CGPoint = .zero
     
+    fileprivate var _imageView = UIImageView()
     fileprivate var assistBigView : UIView = UIView()
     fileprivate var assistSmallView : UIView = UIView()
-    fileprivate var clipView : UIView = UIView()
+    fileprivate var clipView : HBClipView = HBClipView()
     
     fileprivate var scopeLineWidth : CGFloat = 5
     fileprivate var scopeMarginWidth : CGFloat = 30
@@ -60,20 +55,16 @@ class HBShelterView: UIView, CAAnimationDelegate {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .clear
+        backgroundColor = .red
+        _imageView.image = #imageLiteral(resourceName: "lufei")
+        _imageView.frame = frame
+        let tapGesture = UITapGestureRecognizer.init(target: self, action: #selector(tap))
+        _imageView.addGestureRecognizer(tapGesture)
+        addSubview(_imageView)
         
         skipFrame = CGRect.init(x: (frame.width - 100) / 2, y: (frame.height - 100) / 2, width: 100, height: 100)
         centerAnchorPoint = CGPoint.init(x: frame.width / 2, y: frame.height / 2)
         setSkipFrame(frame: frame)
-        
-        scopeLayer.borderWidth = 0
-        scopeLayer.lineWidth = scopeLineWidth
-        scopeLayer.strokeColor = UIColor.blue.cgColor
-        scopeLayer.fillColor = RGB(0x000000).cgColor
-        scopeLayer.fillRule = "even-odd"
-        scopeLayer.path = scopeBezierPath.cgPath
-        scopeLayer.opacity = 0.5
-        layer.addSublayer(scopeLayer)
         
         //assistBigView.backgroundColor = RGBA(0x00ff00, alpha: 0.5)
         assistBigView.backgroundColor = .clear
@@ -83,7 +74,7 @@ class HBShelterView: UIView, CAAnimationDelegate {
         addSubview(assistSmallView)
         
         clipView.backgroundColor = .clear
-        clipView.layer.borderColor = UIColor.red.cgColor
+        clipView.layer.borderColor = UIColor.blue.cgColor
         clipView.layer.borderWidth = scopeLineWidth
         addSubview(clipView)
     }
@@ -93,13 +84,28 @@ class HBShelterView: UIView, CAAnimationDelegate {
         
     }
     
+    @objc func tap() -> () {
+        print("❤️ ---> tapped")
+    }
+    
     fileprivate var kIfTouched : Bool = false
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        super.hitTest(point, with: event)
+        print("❤️hitTest ---> \(point)")
+        currentTouchPoint = point
+        if assistBigView.frame.contains(point) && !assistSmallView.frame.contains(point) {
+            kIfTouched = true
+            return clipView
+        }else {
+            kIfTouched = false
+            return _imageView
+        }
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
-        if let _touch = touches.first {
-            currentTouchPoint = _touch.location(in: self)
-            if assistBigView.frame.contains(currentTouchPoint) && !assistSmallView.frame.contains(currentTouchPoint) {
-                kIfTouched = true
+        if let _ = touches.first {
+            if kIfTouched {
                 /// 确定起始类型
                 if currentTouchPoint.x <= assistSmallView.frame.minX {
                     if currentTouchPoint.y <= assistSmallView.frame.minY {
@@ -122,14 +128,11 @@ class HBShelterView: UIView, CAAnimationDelegate {
                 }else {
                     skipInitialType = .bottom
                 }
-            }else {
-                kIfTouched = false
             }
         }
     }
     
     fileprivate var skipFrame : CGRect = .zero
-    //fileprivate let mostLargeMargin : CGFloat = 2 * (scopeMarginWidth + scopeLineWidth)
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesMoved(touches, with: event)
         if !kIfTouched { return }
@@ -159,7 +162,7 @@ class HBShelterView: UIView, CAAnimationDelegate {
                 if skipFrame.width + xDisValue < mostLargeMargin || skipFrame.height - yDisValue < mostLargeMargin {
                     break
                 }
-                skipFrame = CGRect.init(x: skipFrame.origin.x + xDisValue, y: skipFrame.origin.y + yDisValue, width: skipFrame.width + xDisValue, height: skipFrame.height - yDisValue)
+                skipFrame = CGRect.init(x: skipFrame.origin.x, y: skipFrame.origin.y + yDisValue, width: skipFrame.width + xDisValue, height: skipFrame.height - yDisValue)
                 break
             case .left:
                 if skipFrame.width - xDisValue < mostLargeMargin || skipFrame.height < mostLargeMargin {
@@ -203,49 +206,56 @@ class HBShelterView: UIView, CAAnimationDelegate {
     }
     
     func setSkipFrame(frame: CGRect) -> () {
-        clipBezierPath = UIBezierPath.init(rect: skipFrame)
-        scopeBezierPath = UIBezierPath.init(rect: frame)
-        scopeBezierPath.append(clipBezierPath)
-        scopeBezierPath.usesEvenOddFillRule = true
         assistBigView.frame = CGRect.init(x: skipFrame.origin.x - scopeMarginWidth, y: skipFrame.origin.y - scopeMarginWidth, width: skipFrame.width + 2 * scopeMarginWidth, height: skipFrame.height + 2 * scopeMarginWidth)
         assistSmallView.frame = CGRect.init(x: skipFrame.origin.x + scopeMarginWidth + scopeLineWidth, y: skipFrame.origin.y + scopeMarginWidth + scopeLineWidth, width: skipFrame.width - 2 * (scopeMarginWidth + scopeLineWidth), height: skipFrame.height - 2 * (scopeMarginWidth + scopeLineWidth))
         clipView.frame = skipFrame
-        scopeLayer.path = scopeBezierPath.cgPath
     }
     
     fileprivate var animationDuration : TimeInterval = 0.25
     func resetToCenter() -> () {
         skipFrame = CGRect.init(x: (bounds.width - skipFrame.width) / 2, y: (bounds.height - skipFrame.height) / 2, width: skipFrame.width, height: skipFrame.height)
-        clipBezierPath = UIBezierPath.init(rect: skipFrame)
-        scopeBezierPath = UIBezierPath.init(rect: bounds)
-        scopeBezierPath.append(clipBezierPath)
-        scopeBezierPath.usesEvenOddFillRule = true
         UIView.animate(withDuration: animationDuration, delay: 0, options: .curveEaseInOut, animations: {
             self.assistBigView.frame = CGRect.init(x: self.skipFrame.origin.x - self.scopeMarginWidth, y: self.skipFrame.origin.y - self.scopeMarginWidth, width: self.skipFrame.width + 2 * self.scopeMarginWidth, height: self.skipFrame.height + 2 * self.scopeMarginWidth)
             self.assistSmallView.frame = CGRect.init(x: self.skipFrame.origin.x + self.scopeMarginWidth + self.scopeLineWidth, y: self.skipFrame.origin.y + self.scopeMarginWidth + self.scopeLineWidth, width: self.skipFrame.width - 2 * (self.scopeMarginWidth + self.scopeLineWidth), height: self.skipFrame.height - 2 * (self.scopeMarginWidth + self.scopeLineWidth))
             self.clipView.frame = self.skipFrame
         })
-        let pathAnimation = CABasicAnimation.init(keyPath: "path")
-        pathAnimation.toValue = scopeBezierPath.cgPath
-        pathAnimation.isRemovedOnCompletion = false
-        pathAnimation.fillMode = kCAFillModeForwards
-        pathAnimation.duration = animationDuration
-        pathAnimation.delegate = self
-        scopeLayer.add(pathAnimation, forKey: "path_animation")
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+class HBClipView: UIView {
     
-    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
-        if let _anim = anim as? CABasicAnimation {
-            if _anim.keyPath == "path" {
-                scopeLayer.path = scopeBezierPath.cgPath
-                scopeLayer.removeAllAnimations()
-            }
-        }
+    fileprivate var scopeBezierPath : UIBezierPath = UIBezierPath()
+    fileprivate var clipBezierPath : UIBezierPath = UIBezierPath()
+    fileprivate var scopeLayer : CAShapeLayer = CAShapeLayer()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        scopeLayer.borderWidth = 0
+        scopeLayer.lineWidth = 5
+        scopeLayer.strokeColor = UIColor.clear.cgColor
+        scopeLayer.fillColor = RGB(0x000000).cgColor
+        scopeLayer.fillRule = "even-odd"
+        scopeLayer.path = scopeBezierPath.cgPath
+        scopeLayer.opacity = 0.5
+        layer.addSublayer(scopeLayer)
     }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let wholeRect = CGRect.init(x: -UIScreen.main.bounds.width, y: -UIScreen.main.bounds.height, width: 2 * UIScreen.main.bounds.width, height: 2 * UIScreen.main.bounds.height)
+        clipBezierPath = UIBezierPath.init(rect: wholeRect)
+        scopeBezierPath = UIBezierPath.init(rect: bounds)
+        scopeBezierPath.append(clipBezierPath)
+        scopeBezierPath.usesEvenOddFillRule = true
+        scopeLayer.path = scopeBezierPath.cgPath
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
 
 /**
